fb940c935938a6b30976b51f5388fdb93f8474b7
riki0723 <69281517+riki0723@users.noreply.github.com>
2020-12-16 22:15:25 +0900

    マイグレーションの編集

--- db/migrate/20201216130750_create_tags.rb
+++ db/migrate/20201216130750_create_tags.rb
@@ -1,7 +1,8 @@
 class CreateTags < ActiveRecord::Migration[6.0]
   def change
     create_table :tags do |t|
-
+      t.string :name, null:false, uniqueness: true
+      # 「uniqueness: true」重複避けるための制約
       t.timestamps
     end
   end
