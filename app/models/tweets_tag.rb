0c861f04aec34bff2706fa456d81166d35b0ec61
riki0723 <69281517+riki0723@users.noreply.github.com>
2020-12-16 22:30:38 +0900

    Formオブジェクトの導入

---
+++ app/models/tweets_tag.rb
@@ -0,0 +1,25 @@
+class TweetsTag
+
+  # Formオブジェクトは、1つのフォーム送信で複数のモデルを更新するときに使用するツールです。
+  # 自分で定義したクラスをモデルのように扱うことができます。
+  # このFormオブジェクトは、「ActiveModel::Model」というモジュールを読み込むことで使うことができます。
+
+  # 「ActiveModel::Model」とは、Active Recordの場合と同様に「form_for」や「render」などのヘルパーメソッドを使えるようになるツールです。
+  # また、「モデル名の調査」や「バリデーション」の機能も使えるようになります。
+
+  include ActiveModel::Model
+  attr_accessor :message, :name
+
+  with_options presence: true do
+    validates :message
+    validates :name
+  end
+
+  def save
+    tweet = Tweet.create(message: message)
+    tag = Tag.create(name: name)
+
+    TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
+  end
+
+end
